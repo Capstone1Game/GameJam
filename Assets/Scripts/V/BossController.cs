613d6aeea58b51954c6b42d417cbd3e53f0306c0
ㅇㅅㅇ <jhy041020@handong.ac.kr>
2025-07-28 19:00:20 +0900

    Add BossController.cs

---
+++ Assets/Scripts/V/BossController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    public GameObject prefab;
+    public BossData[] data;
+    public void CreateBoss(int id)
+    {
+        GameObject instantBoss = Instantiate(prefab);
+        if (instantBoss.TryGetComponent(out Boss boss))
+        {
+            boss.data = data[id];
+            boss.Init();
+        }
+    }
+    void Start()
+    {
+        CreateBoss(1);
+    }
+}
