7447ba35789156ce9b09f0004ed154bed51d06bb
ㅇㅅㅇ <jhy041020@handong.ac.kr>
2025-07-28 19:00:08 +0900

    Add BossData.cs

---
+++ Assets/Scripts/V/BossData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Boss", menuName = "Scriptable Object / BossData")]
+public class BossData : ScriptableObject
+{
+    public int id;
+    public string bossName;
+    public Vector3 location; // 생성할 보스 위치
+    public Sprite sprite;
+    public float damage;
+    public float maxHealth; // 최대 체력
+    public float moveSpeed; // 이동 속도
+}
