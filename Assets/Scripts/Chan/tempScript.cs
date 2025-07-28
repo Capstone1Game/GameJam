927dad5d7e5504cbe7f9bd0eb1a759c36e985824
Gamdict-Zombie <rlawncks0103@naver.com>
2025-07-28 18:55:05 +0900

    마우스 이동에 따른 무기 이동
    마우스 이동에 따른 무기 이동 구현

---
+++ Assets/Scripts/Chan/tempScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempScript : MonoBehaviour
+{
+            //찌르기 구현을 위한 움직임
+            /*if (isLeft)
+            {
+                rotZ = Mathf.Clamp(rotZ, 90f, 180f);
+                if (dir.x < 0)
+                {
+                    transform.position = parentTransform.position + dir;
+                }
+                else
+                {
+                    transform.position = parentTransform.position;                    
+                }
+            }
+            else
+            {
+                rotZ = Mathf.Clamp(rotZ, 0f, 90f);
+                if (dir.x < 0)
+                {
+                    transform.position = parentTransform.position;
+                }
+                else
+                {
+                    transform.position = parentTransform.position + dir;
+                }
+            }*/
+}
