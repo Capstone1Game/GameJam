340ab4ea5132d81d35922ae328a2d73e69a3569f
ㅇㅅㅇ <jhy041020@handong.ac.kr>
2025-07-29 17:41:04 +0900

    Update BossController.cs

--- Assets/Scripts/V/BossController.cs
+++ Assets/Scripts/V/BossController.cs
@@ -7,18 +7,37 @@ public class BossController : MonoBehaviour
 {
     public GameObject prefab;
     public BossData[] data;
+    private Boss boss;
     public void CreateBoss(int id)
     {
         GameObject instantBoss = Instantiate(prefab);
-        if (instantBoss.TryGetComponent(out Boss boss))
+        if (boss = instantBoss.GetComponent<Boss>())
         {
             instantBoss.name = $"Boss {id}";
             boss.Init(data[id]);
+            StartBossRoutine(id);
         }
     }
 
-    void Start()
+    public void StartBossRoutine(int id)
     {
-        CreateBoss(0);
+        switch (id)
+        {
+            case 0:
+                Debug.Log(boss.name);
+                StartCoroutine(FireBullet(boss.bullet));
+                break;
+        }
+    }
+
+    IEnumerator FireBullet(GameObject bullet)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2f);
+            GameObject instantBullet = Instantiate(bullet);
+            instantBullet.transform.SetParent(boss.transform);
+            StartCoroutine(instantBullet.GetComponent<Bullet>().FireBullet(instantBullet.transform.position, boss.target.position));
+        }
     }
 }
