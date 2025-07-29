242bcfe94dc8f4e4326074d85b96b78bc1013a78
정해윤 <jhy041020@handong.ac.kr>
2025-07-30 06:15:57 +0900

    Update Follow.cs

--- Assets/Scripts/V/Follow.cs
+++ Assets/Scripts/V/Follow.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class Follow : MonoBehaviour
 {
     RectTransform rect;
+    GameObject target;
+    float dir;
     void Start()
     {
         rect = GetComponent<RectTransform>();
+        TargetInit();
+        gameObject.SetActive(true);
+    }
+
+    void TargetInit()
+    {
+        switch (gameObject.name)
+        {
+            case "BossHealth":
+                target = GameManager.instance.boss;
+                dir = -3f;
+                break;
+            case "PlayerHealth":
+                target = GameManager.instance.player;
+                dir = 0.8f;
+                break;
+        }
     }
 
     void FixedUpdate()
     {
-        if (GameManager.instance.boss == null) return;
-        Vector3 pos = GameManager.instance.boss.transform.position;
-        pos.y = pos.y - 3f;
+        if (target == null) return;
+        Vector3 pos = target.transform.position;
+        pos.y = pos.y + dir;
         rect.position = Camera.main.WorldToScreenPoint(pos);
     }
 }
