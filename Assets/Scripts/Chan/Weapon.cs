f348d84c56a86935ad2f5adf7b7237098ecad71b
Gamdict-Zombie <rlawncks0103@naver.com>
2025-07-29 12:37:32 +0900

    무기 시스템 구현
    Weapon.sctipt작성
    PlayerMouse수정

---
+++ Assets/Scripts/Chan/Weapon.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private int weaponDamage = 10;
+    private PlayerMouse parentMouse;
+    public int damage;
+    void Start()
+    {
+        parentMouse = transform.parent.GetComponent<PlayerMouse>();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boss")
+        {
+            damage = CalculateDamage();
+            Debug.Log(damage);
+        }
+    }
+    //무기의 대미지를 받고 움직이는 속도에 따라 0~1의
+    int CalculateDamage()
+    {
+        float velocity = parentMouse.velocity;
+        int temp = (int)((float)weaponDamage * velocity);
+        if (temp < 1) temp = 1;
+        return temp;
+    }
+}
