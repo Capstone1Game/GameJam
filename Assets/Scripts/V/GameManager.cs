a824d077391f0362cc94ba14335d9fd0e0f3b5ac
Fungineer12 <39977034+Fungineer12@users.noreply.github.com>
2025-07-30 11:54:59 +0900

    #104

--- Assets/Scripts/V/GameManager.cs
+++ Assets/Scripts/V/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,6 +9,11 @@ public class GameManager : MonoBehaviour
     public GameObject player; // 임시 코드
     public GameObject boss;
     public BossController bossController;
+
+    public GameObject gameOverText;
+    public GameObject retryButton;
+    private bool isGameOver = false;
+
     void Awake()
     {
         instance = this;
@@ -16,5 +22,24 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         bossController.CreateBoss(0); // 임시 코드
+    }
+
+
+    
+
+    public void GameOver()
+    {
+        if (isGameOver) return;
+
+        isGameOver = true;
+        gameOverText.SetActive(true);
+        retryButton.SetActive(true);
+        
+    }
+
+    public void Retry()
+    {
+        Debug.Log("Retry called");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
